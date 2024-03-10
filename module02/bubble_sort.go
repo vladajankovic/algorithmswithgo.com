0aa3373a263c8607c7b7aaec070a72861d77635c
vladajankovic <vladajankovic1999@gmail.com>
2024-03-10 11:29:47 +0100

    Update sorting algorithms
    Implement bubble sort and insertion sort

--- module02/bubble_sort.go
+++ module02/bubble_sort.go
@@ -2,4 +2,16 @@ package module02
 
 // BubbleSortInt will sort a list of integers using the bubble sort algorithm.
 func BubbleSortInt(list []int) {
+	for i := 0; i < len(list); i++ {
+		sorted := true
+		for j := 1; j < len(list)-i; j++ {
+			if list[j-1] > list[j] {
+				list[j-1], list[j] = list[j], list[j-1]
+				sorted = false
+			}
+		}
+		if sorted {
+			break
+		}
+	}
 }
--- module02/insertion_sort.go
+++ module02/insertion_sort.go
@@ -3,4 +3,9 @@ package module02
 // InsertionStortInt will sort a list of integers using the insertion sort
 // algorithm.
 func InsertionStortInt(list []int) {
+	for i := 1; i < len(list); i++ {
+		for j := i; j > 0 && list[j] < list[j-1]; j-- {
+			list[j-1], list[j] = list[j], list[j-1]
+		}
+	}
 }
